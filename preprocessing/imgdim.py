2001185bdc5b9f657667ae0b7e041e3492615ed9
mtang4 <52719555+mtang4@users.noreply.github.com>
2019-07-12 10:57:07 -0400

    Update imgdim.py

--- preprocessing/imgdim.py
+++ preprocessing/imgdim.py
@@ -3,7 +3,11 @@ import os
 
 filePath='/vulcan/scratch/mtang/datasets/ABIDE/min_process'
 fileList=os.listdir(filePath)
-for i in range(0, 1035):
-    img=nib.load(filePath+'/'+fileList[i])
-    imgData=img.get_fdata()
-    print('dim: '+str(imgData.shape))
+img=nib.load('/vulcan/scratch/mtang/datasets/ABIDE/minimal.nii.gz')
+imgData=img.get_fdata()
+print('dim: '+str(imgData.shape))
+
+# for i in range(0, 1035):
+#    img=nib.load(filePath+'/'+fileList[i])
+#    imgData=img.get_fdata()
+#    print('dim: '+str(imgData.shape))
