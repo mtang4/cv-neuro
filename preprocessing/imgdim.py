198fab577b3cd536debbbca5478a29a32ceccb5c
mtang4 <52719555+mtang4@users.noreply.github.com>
2019-07-12 10:30:36 -0400

    Update imgdim.py

--- preprocessing/imgdim.py
+++ preprocessing/imgdim.py
@@ -4,6 +4,6 @@ import os
 filePath='/vulcan/scratch/mtang/datasets/ABIDE/min_process'
 fileList=os.listdir(filePath)
 for i in range(0, 1035):
-    img=nib.load(filPath+fileList[i])
+    img=nib.load(filePath+fileList[i])
     imgData=img.get_fdata()
     print('dim: '+str(imgData.shape))
