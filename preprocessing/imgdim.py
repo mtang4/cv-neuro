2277c3b6b7913a9a0f32ea1cc9c5362bfdb3a83a
mtang4 <52719555+mtang4@users.noreply.github.com>
2019-07-12 10:27:32 -0400

    Update imgdim.py

--- preprocessing/imgdim.py
+++ preprocessing/imgdim.py
@@ -1,1 +1,9 @@
 import nibabel as nib
+import os
+
+filePath='/vulcan/scratch/mtang/datasets/ABIDE/min_process'
+fileList=os.listdir(filePath)
+for i in range(0, 1035):
+    img=nib.load(filPath+fileList[i])
+    imgData=img.get_fdata()
+    print('dim: '+str(imgData.shape))
